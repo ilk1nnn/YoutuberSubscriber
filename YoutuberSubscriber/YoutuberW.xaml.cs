a2e5afeecaa8e231a5742434a5774cfb91fb82ba
Sule_jr92 <Sule_jr92@STHQ0129-04>
2022-10-16 17:19:15 +0400

    Add project files.

---
+++ YoutuberSubscriber/YoutuberW.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YoutuberSubscriber
+{
+    /// <summary>
+    /// Interaction logic for YoutuberW.xaml
+    /// </summary>
+    public partial class YoutuberW : Window
+    {
+        public YoutuberW()
+        {
+            InitializeComponent();
+        }
+
+
+        class Subscriber
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        List<Subscriber> subscribers = new List<Subscriber>
+        {
+
+
+            new Subscriber
+            {
+                Username = "admin1",
+                Password ="admin1"
+            },
+            new Subscriber
+            {
+                Username = "admin2",
+                Password="admin2"
+            },
+            new Subscriber
+            {
+                Username = "admin3",
+                Password="admin3"
+            },
+            new Subscriber
+            {
+                Username="admin4",
+                Password = "admin4"
+            }
+
+        };
+
+        private void ShowSub(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
